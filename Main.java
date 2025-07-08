f6ec111e77299560170f00925c81a83554aec776
anujmaurya6 <maurya913639@gmail.com>
2025-07-08 23:38:31 +0530

    javascript revision

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    String str ="anuj maurya";
+    int count=str.length();
+    System.out.println("total characters"+count);
+    
+    }
+}
---
+++ prac.js
@@ -0,0 +1,9 @@
+const person={
+    name:"anuj;",
+    age:89,
+    adddress:"dubai",
+greet (){
+    console.log("hello my name is "+this.name+"and i am "+this.age+'years old');
+}
+};
+person.greet();
